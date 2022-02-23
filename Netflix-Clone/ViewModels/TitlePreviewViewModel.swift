1e8c625b37ef26eb72ec0f1cff9331bbc6037aac
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-23 14:41:13 +0300

    ViewModels - TitlePriewViewModel

---
+++ Netflix-Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix-Clone
+//
+//  Created by Abdirizak Hassan on 2/22/22.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
